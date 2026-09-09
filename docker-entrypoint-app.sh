8be947d1eec735f91dbabff8f8e1b14e0cf16bac
Naveen Sharma <naveenbhardwaj5828@gmail.com>
2026-09-09 23:44:07 +0530

    Test automatic rollback on bad production release

--- docker-entrypoint-app.sh
+++ docker-entrypoint-app.sh
@@ -4,8 +4,16 @@ set -e
 APP_VERSION="${APP_VERSION:-unknown}"
 APP_ENVIRONMENT="${APP_ENVIRONMENT:-unknown}"
 
+# Controlled rollback test:
+# CI behaves normally, but BLUE/GREEN intentionally shows a bad version.
+if [ "$APP_ENVIRONMENT" = "CI" ]; then
+    DISPLAY_VERSION="$APP_VERSION"
+else
+    DISPLAY_VERSION="BROKEN-RELEASE"
+fi
+
 sed \
-  -e "s/__APP_VERSION__/${APP_VERSION}/g" \
+  -e "s/__APP_VERSION__/${DISPLAY_VERSION}/g" \
   -e "s/__APP_ENVIRONMENT__/${APP_ENVIRONMENT}/g" \
   /usr/share/nginx/html/index.template.html \
   > /usr/share/nginx/html/index.html
